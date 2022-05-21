aa9666796cff10ed80589b0811d87a10bb284f51
mhhan <hanminhee1025@gmail.com>
2022-05-21 22:38:43 +0900

    스프링부트 초기설정 push

---
+++ 02.개발/studyroom/src/main/java/com/toy/studyroom/StudyroomApplication.java
@@ -0,0 +1,13 @@
+package com.toy.studyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyroomApplication.class, args);
+	}
+
+}
